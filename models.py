90ccaa58ee66ed1d73e5cf42b33529bc46ee7671
dwaynxz <dwayne604012@gmail.com>
2025-11-02 16:14:35 +0000

    feat: create Cart table

--- models.py
+++ models.py
@@ -3,9 +3,18 @@ from sqlalchemy.orm import backref
 from main import db
 
 class User(db.Model):
+    __tablename__ = "Users"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     forename = db.Column(db.String, nullable=False)
     lastname = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
-    cart = db.relationship("cart", backref="cart", lazy=True)
+    cart = db.relationship("cart", backref="user", lazy=True)
+
+class Cart(db.Model):
+    __tablename__ = "Carts"
+    user_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
+    paid = db.Column(db.String, nullable=False)
+    cart_item = db.relationship("CartItem", backref="cart", lazy=True)
+
+
