ac1f08a533bd8774ff5fe648e2e5727861b38006
dwaynxz <dwayne604012@gmail.com>
2025-11-02 17:10:35 +0000

    fix, feat: correct typos, create cart item and menu item tables

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-from sqlalchemy.orm import backref
-
 from main import db
+from sqlalchemy import Numeric
 
 class User(db.Model):
     __tablename__ = "Users"
@@ -14,8 +13,21 @@ class User(db.Model):
 class Cart(db.Model):
     __tablename__ = "Carts"
     cart_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.Foreignkey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"), nullable=False)
     paid = db.Column(db.String, nullable=False)
     cart_item = db.relationship("CartItem", backref="cart", lazy=True)
 
+class CartItem(db.Model):
+    __tablename__ = "Cart_Items"
+    item_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    cart_id = db.Column(db.Integer, db.ForeignKey("Carts.cart_id"), nullable=False)
+    menu_item = db.Column(db.Integer, db.ForeignKey("Menu_Items.item_id"), nullable=False)
+
+class MenuItem(db.Model):
+    __tablename__ = "Menu_Items"
+    item_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(Numeric(5, 2), nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+    cart_item = db.relationship("CartItem", backref="menuitem", lazy=True)
 
