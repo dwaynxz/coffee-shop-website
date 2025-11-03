b3c25a1c46d9b15d8903b233eecd940997cbbe90
dwaynxz <dwayne604012@gmail.com>
2025-11-03 23:11:58 +0000

    feat, fix: add logout route, fix redirect issue by adding  'post' to methods

--- main.py
+++ main.py
@@ -127,27 +127,35 @@ def register():
                                 return redirect(url_for("login"))
     return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if not email:
-        flash("Email field can't be empty", "danger")
-        return redirect(url_for("login"))
-    if not password:
-        flash("Password field can't be empty")
-        return render_template("login.html", email=email)
-    user = User.query.filter_by(email=email).first()
-    if not user:
-        flash("Email doesn't exist. Please register", "danger")
-        return render_template("login.html", email=email)
-    check_password = bcrypt.check_password_hash(user.password, password)
-    if not check_password:
-        flash("Incorrect Password", "danger")
-        return render_template("login.html", email=email)
-    login_user(user)
-    flash("Logged in successfully", "success")
-    return redirect(url_for("menu"))
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if not email:
+            flash("Email field can't be empty", "danger")
+            return redirect(url_for("login"))
+        if not password:
+            flash("Password field can't be empty", "danger")
+            return render_template("login.html", email=email)
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("Email doesn't exist. Please register", "danger")
+            return render_template("login.html", email=email)
+        check_password = bcrypt.check_password_hash(user.password, password)
+        if not check_password:
+            flash("Incorrect Password", "danger")
+            return render_template("login.html", email=email)
+        login_user(user)
+        flash("Logged in successfully", "success")
+        return redirect(url_for("menu"))
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("Logged out successfully", "success")
+    return redirect(url_for("homepage"))
 
 if __name__ == "__main__":
     app.run(debug=True)
