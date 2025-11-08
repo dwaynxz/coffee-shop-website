75ac6749f182242501ed4b18b944f1a8c1601c3c
dwaynxz <dwayne604012@gmail.com>
2025-11-08 17:08:48 +0000

    feat: create admin account on start

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from main import app, db
-from models import MenuItem
+from main import app, db, bcrypt
+from models import MenuItem, User
 from menu import drinks, desserts, breakfast
+import time
 with app.app_context():
     db.create_all()
     print("Database tables created")
-
+    time.sleep(2)
+    admin = User(forename="admin", lastname="123", password=bcrypt.generate_password_hash("admin123"), email="admin123@gmail.com", admin=True)
+    db.session.add(admin)
+    db.session.commit()
     for item, value in drinks.items():
             name = value["name"]
             price = value["price"]
